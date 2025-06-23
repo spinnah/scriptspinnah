a5c4b0b2209fda17d83ff0fb6f859e4932e05916
ShastLeLow <34039729+ShastLeLow@users.noreply.github.com>
2025-06-23 12:02:09 -0400

    Initial Commit

---
+++ ScriptSpinnah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScriptSpinnah
+//
+//  Created by Shawn Starbird on 6/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScriptSpinnah/ScriptSpinnahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptSpinnahApp.swift
+//  ScriptSpinnah
+//
+//  Created by Shawn Starbird on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ScriptSpinnahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
